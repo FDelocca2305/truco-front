c7c59a0d48436bb8815b4d0e68c2253a4c5244f5
Franco Delocca <52047668+FDelocca2305@users.noreply.github.com>
2025-05-09 09:15:18 -0300

    Update TrucoLogo.tsx

--- src/components/navbarElements/TrucoLogo.tsx
+++ src/components/navbarElements/TrucoLogo.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import mate from '../../assets/mate.png';
+import logo from '../../../public/LOGO.png';
 import { Link } from 'react-router-dom';
 import { GameContext } from '../../context/gameContext';
 
@@ -8,8 +8,7 @@ const TrucoLogo: React.FC = () => {
   return (
     <Link to={"/"} className='w-full relative flex justify-center text-text'
       onClick={closeSideBar}>
-      <img src={mate} alt='Logo' className='w-[115px] h-[115px] rotate-6' />
-      <h1 className='absolute bottom-1/2 translate-y-3/4 w-full text-center font-bold text-4xl'>Truco.com</h1>
+      <img src={logo} alt='Logo' className='w-[115px] h-[115px] rotate-6' />
     </Link>
   );
 };
